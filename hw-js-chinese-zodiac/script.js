b1e0cfde5b7dfcca78213d37155b44a9cba768cb
emin0784 <149255276+emin0784@users.noreply.github.com>
2023-12-08 18:22:13 +0100

    Add files via upload

---
+++ hw-js-chinese-zodiac/script.js
@@ -0,0 +1,69 @@
+
+let input = prompt("Birth Year:");
+let result = (input -4) % 12;
+
+if (result===0) {
+
+    alert("Your zodiac is 'Rat'");
+
+}
+else if(result===1) {
+
+    alert("Your zodiac is 'Ox'")
+
+}
+else if(result===2) {
+
+    alert("Your zodiac is 'Tigar'")
+
+}
+else if(result===3) {
+
+    alert("Your zodiac is 'Rabbit'")
+
+}
+else if(result===4) {
+
+    alert("Your zodiac is 'Dragon'")
+
+}
+else if(result===5) {
+
+    alert("Your zodiac is 'Snake'")
+
+}
+else if(result===6) {
+
+    alert("Your zodiac is 'Horse'")
+
+}
+else if(result===7) {
+
+    alert("Your zodiac is 'Goat'")
+
+}
+else if(result===8) {
+
+    alert("Your zodiac is 'Monkey'")
+
+}
+else if(result===9) {
+
+    alert("Your zodiac is 'Rooster'")
+
+}
+else if(result===10) {
+
+    alert("Your zodiac is 'Dog'")
+
+}
+else if(result===11) {
+
+    alert("Your zodiac is 'Pig'")
+
+}
+else  {
+
+    alert("Enter valid number")
+
+}
